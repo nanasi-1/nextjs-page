3ca0336ba0f1e91e332c67f8c1e0e03e62d7d376
nanasi-1 <test_email>
2024-02-04 14:19:13 +0900

    Reactチュートリアルがやりたい

---
+++ src/app/tic-tac-toe/page.tsx
@@ -0,0 +1,14 @@
+import './globals.css';
+
+export default function () {
+    return (
+        <main>
+            <h1>三目並べ</h1>
+            <p>
+                <a href="https://ja.react.dev/learn/tutorial-tic-tac-toe">Reactチュートリアル</a>
+                のやつを作りたいと思ってる<br />
+                今のところ思ってるだけ<br />
+            </p>
+        </main>
+    );
+}
